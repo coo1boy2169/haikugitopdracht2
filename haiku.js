c760defb4d8a55fa1c6c06beb15145c9a4b80f9b
Olgun Hristov <36889@ma-web.nl>
2023-10-09 12:08:04 +0200

    com

--- haiku.js
+++ haiku.js
@@ -2,7 +2,24 @@
 
 //plak hier de haiku functions onder elkaar
 
-
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
 
 
 //laat de code hieronder met rust
